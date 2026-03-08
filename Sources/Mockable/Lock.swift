c30282aea2cbb05cd6ede162fc019cdeefcd8933
Yusuke Morishita <hello@yusuke.ca>
2026-03-09 08:25:52 +0900

    fix: wrap `sending` keyword in `#if compiler(>=6.0)` for Swift 5.x compatibility

--- Sources/Mockable/Lock.swift
+++ Sources/Mockable/Lock.swift
@@ -15,19 +15,35 @@ public final class LegacyLock<Value>: @unchecked Sendable {
         self._value = initialValue
     }
 
+    #if compiler(>=6.0)
     @discardableResult
     public func withLock<Result>(_ body: (inout sending Value) throws -> sending Result) rethrows -> sending Result {
         _lock.lock()
         defer { _lock.unlock() }
         return try body(&_value)
     }
+    #else
+    @discardableResult
+    public func withLock<Result>(_ body: (inout Value) throws -> Result) rethrows -> Result {
+        _lock.lock()
+        defer { _lock.unlock() }
+        return try body(&_value)
+    }
+    #endif
 }
 
 private class _LockBoxBase<Value>: @unchecked Sendable {
+    #if compiler(>=6.0)
     @discardableResult
     func withLock<Result>(_ body: (inout sending Value) throws -> sending Result) rethrows -> sending Result {
         fatalError("Unimplemented lock box")
     }
+    #else
+    @discardableResult
+    func withLock<Result>(_ body: (inout Value) throws -> Result) rethrows -> Result {
+        fatalError("Unimplemented lock box")
+    }
+    #endif
 }
 
 private final class LegacyLockBox<Value>: _LockBoxBase<Value>, @unchecked Sendable {
@@ -37,12 +53,18 @@ private final class LegacyLockBox<Value>: _LockBoxBase<Value>, @unchecked Sendab
         self._lock = LegacyLock(initialValue)
     }
 
+    #if compiler(>=6.0)
     override func withLock<Result>(_ body: (inout sending Value) throws -> sending Result) rethrows -> sending Result {
         try _lock.withLock(body)
     }
+    #else
+    override func withLock<Result>(_ body: (inout Value) throws -> Result) rethrows -> Result {
+        try _lock.withLock(body)
+    }
+    #endif
 }
 
-#if canImport(Synchronization)
+#if compiler(>=6.0) && canImport(Synchronization)
 @available(macOS 15.0, iOS 18.0, tvOS 18.0, watchOS 11.0, *)
 private final class MutexLockBox<Value>: _LockBoxBase<Value>, @unchecked Sendable {
     private let _lock: Mutex<Value>
@@ -64,7 +86,7 @@ public final class MockableLock<Value>: @unchecked Sendable {
     private let _box: _LockBoxBase<Value>
 
     public init(_ initialValue: Value) {
-        #if canImport(Synchronization)
+        #if compiler(>=6.0) && canImport(Synchronization)
         if #available(macOS 15.0, iOS 18.0, tvOS 18.0, watchOS 11.0, *) {
             self._box = MutexLockBox(initialValue)
         } else {
@@ -75,8 +97,15 @@ public final class MockableLock<Value>: @unchecked Sendable {
         #endif
     }
 
+    #if compiler(>=6.0)
     @discardableResult
     public func withLock<Result>(_ body: (inout sending Value) throws -> sending Result) rethrows -> sending Result {
         try _box.withLock(body)
     }
+    #else
+    @discardableResult
+    public func withLock<Result>(_ body: (inout Value) throws -> Result) rethrows -> Result {
+        try _box.withLock(body)
+    }
+    #endif
 }
